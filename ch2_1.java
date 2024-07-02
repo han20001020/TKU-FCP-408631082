3434128c46963afb94ae456b1a227b6a2f4e6c41
han20001020 <408631082@o365.tku.edu.tw>
2024-07-02 11:54:08 +0800

    我的第一支JAVA程式

---
+++ ch2_1.java
@@ -0,0 +1,14 @@
+public class ch2_1 {    //定義public 類別ch2_1
+    
+    public static void main(String[] args) {  //main() 函數，主程式開始
+        int num1=2;   //宣告整數num
+        int num2=3;   //宣告整數num
+        float num3=4; //宣告浮點數num
+        String num4="五";
+
+        System.out.println(num1+"cats are running");  //印出字串及變數內容  
+        System.out.println(num2+"dogs are runnimg");  //印出字串及變數內容  
+        System.out.println(num3+"birds are runnimg"); //印出字串及變數內容 
+        System.out.println(num4+"tigers are runnimg"); //印出字串及變數內容 
+    }
+}
