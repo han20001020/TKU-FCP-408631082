63bfae0c460c8ed4bf51cb0daf4a64824cc5373e
han20001020 <408631082@o365.tku.edu.tw>
2024-07-04 22:47:54 +0800

    hm

---
+++ hm_02.java
@@ -0,0 +1,7 @@
+public class hm_02 {
+    public static void main(String[] args) {
+        System.out.println("Name: HANHAN");
+        System.out.println("Age: 23");
+        System.out.println("Hobbies: Reading, Traveling, Coding");
+    }
+}
---
+++ hm_05.java
@@ -0,0 +1,18 @@
+public class hm_05 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.printf("Sum: %d%n", sum);
+        System.out.printf("Difference: %d%n", difference);
+        System.out.printf("Product: %d%n", product);
+        System.out.printf("Quotient: %d%n", quotient);
+        System.out.printf("Remainder: %d%n", remainder);
+    }
+}
